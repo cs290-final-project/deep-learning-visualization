4d2b1b0c85210328fa8f8748c2624325e6df3f10
ShaurGaur <shauryavrat@live.com>
2020-06-05 14:12:30 -0700

    made Community take in parameters

--- client/src/Community.tsx
+++ client/src/Community.tsx
@@ -14,87 +14,8 @@ export interface Network {
 export interface Networks {
     nets: Network[];
 }
-const Community: React.FC = () => {
-    const [networks, useNetworks] = useState([
-        {
-            name: "Test Network 1",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque mi et ullamcorper eleifend. Curabitur eu neque sollicitudin, pulvinar purus vitae, posuere metus.",
-            creator: "Shaurya Gaur",
-            id: "5198198098",
-            layers: [
-                {
-                    id: 1,
-                    type: "Conv2d",
-                    width: 128,
-                    activation: "ReLU",
-                },
-                {
-                    id: 2,
-                    type: "MaxPool2d",
-                    width: 64,
-                    activation: "Sigmoid",
-                },
-                {
-                    id: 3,
-                    type: "Linear",
-                    width: 10,
-                    activation: "Softmax",
-                },
-            ],
-        },
-        {
-            name: "Test Network 2",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque mi et ullamcorper eleifend. Curabitur eu neque sollicitudin, pulvinar purus vitae, posuere metus.",
-            creator: "Andrew Pynch",
-            id: "5198198098",
-            layers: [
-                {
-                    id: 1,
-                    type: "Conv2d",
-                    width: 128,
-                    activation: "ReLU",
-                },
-                {
-                    id: 2,
-                    type: "MaxPool2d",
-                    width: 64,
-                    activation: "Sigmoid",
-                },
-                {
-                    id: 3,
-                    type: "Linear",
-                    width: 10,
-                    activation: "Softmax",
-                },
-            ],
-        },
-        {
-            name: "Test Network 3",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque mi et ullamcorper eleifend. Curabitur eu neque sollicitudin, pulvinar purus vitae, posuere metus.",
-            creator: "Philip Bindemann",
-            id: "5198198098",
-            layers: [
-                {
-                    id: 1,
-                    type: "Conv2d",
-                    width: 128,
-                    activation: "ReLU",
-                },
-                {
-                    id: 2,
-                    type: "MaxPool2d",
-                    width: 64,
-                    activation: "Sigmoid",
-                },
-                {
-                    id: 3,
-                    type: "Linear",
-                    width: 10,
-                    activation: "Softmax",
-                },
-            ],
-        },
-    ]);
+const Community: React.FC<Networks> = (nets) => {
+    const [networks, useNetworks] = useState(nets);
 
     const renderNetwork = (network: {
         name: string,
@@ -118,7 +39,7 @@ const Community: React.FC = () => {
         <>
             <Container>
                 <h1>Community Networks</h1>
-                {networks.map((net) => renderNetwork(net))}
+                {networks.nets.map((net) => renderNetwork(net))}
             </Container>
         </>
     );
