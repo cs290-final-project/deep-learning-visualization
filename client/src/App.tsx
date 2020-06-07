12542c5039c0825a081871caa4375eba2c2116e8
Phillip Bindeman <9490743+bindeman@users.noreply.github.com>
2020-06-07 01:20:46 -0700

    community networks fetched from db

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,87 +5,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import Network from "./Components/network/Network";
 import Community from "./Community"
 
-const networks = [
-    {
-        name: "Test Network 1",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque mi et ullamcorper eleifend. Curabitur eu neque sollicitudin, pulvinar purus vitae, posuere metus.",
-        creator: "Shaurya Gaur",
-        id: "5198198098",
-        layers: [
-            {
-                id: 1,
-                type: "Conv2d",
-                width: 128,
-                activation: "ReLU",
-            },
-            {
-                id: 2,
-                type: "MaxPool2d",
-                width: 64,
-                activation: "Sigmoid",
-            },
-            {
-                id: 3,
-                type: "Linear",
-                width: 10,
-                activation: "Softmax",
-            },
-        ],
-    },
-    {
-        name: "Test Network 2",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque mi et ullamcorper eleifend. Curabitur eu neque sollicitudin, pulvinar purus vitae, posuere metus.",
-        creator: "Andrew Pynch",
-        id: "5198198098",
-        layers: [
-            {
-                id: 1,
-                type: "Conv2d",
-                width: 128,
-                activation: "ReLU",
-            },
-            {
-                id: 2,
-                type: "MaxPool2d",
-                width: 64,
-                activation: "Sigmoid",
-            },
-            {
-                id: 3,
-                type: "Linear",
-                width: 10,
-                activation: "Softmax",
-            },
-        ],
-    },
-    {
-        name: "Test Network 3",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque mi et ullamcorper eleifend. Curabitur eu neque sollicitudin, pulvinar purus vitae, posuere metus.",
-        creator: "Philip Bindemann",
-        id: "5198198098",
-        layers: [
-            {
-                id: 1,
-                type: "Conv2d",
-                width: 128,
-                activation: "ReLU",
-            },
-            {
-                id: 2,
-                type: "MaxPool2d",
-                width: 64,
-                activation: "Sigmoid",
-            },
-            {
-                id: 3,
-                type: "Linear",
-                width: 10,
-                activation: "Softmax",
-            },
-        ],
-    },
-];
-
 function App() {
     return (
         <div className="App">
@@ -97,7 +16,7 @@ function App() {
                         </DndProvider>
                     </Col>
                     <Col md={4}>
-                        <Community nets={networks} />
+                        <Community />
                     </Col>
                 </Row>
             </Container>
--- client/src/Community.tsx
+++ client/src/Community.tsx
@@ -1,48 +1,62 @@
-import React, { useCallback, useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ComNetwork from "./Components/network/ComNetwork";
 
-export interface Layer { id: number; type: string; width: number; activation: string }
 
-export interface Network {
-    name: string,
-    description: string,
-    creator: string,
-    id: string,
-    layers: Layer[],
-}
-export interface Networks {
-    nets: Network[];
-}
-const Community: React.FC<Networks> = (nets) => {
-    const [networks, useNetworks] = useState(nets);
-
-    const renderNetwork = (network: {
-        name: string,
-        description: string,
-        creator: string,
-        id: string,
-        layers: Layer[],
-    }) => {
-        return (
-            <ComNetwork
-                name={network.name}
-                description={network.description}
-                creator={network.creator}
-                id={network.id}
-                layers={network.layers}
-            />
-        );
-    };
-
-    return (
-        <>
-            <Container>
-                <h1>Community Networks</h1>
-                {networks.nets.map((net) => renderNetwork(net))}
-            </Container>
-        </>
-    );
-};
-
-export default Community;
+  import React, { useCallback, useState, useEffect } from "react";
+  import { Container, Row, Col } from "react-bootstrap";
+  import ComNetwork from "./Components/network/ComNetwork";
+  import axios from 'axios';
+  
+  export interface Layer { id: number; type: string; width: number; activation: string }
+  
+  export interface Network {
+      name: string,
+      description: string,
+      creator: string,
+      id: string,
+      layers: Layer[],
+  }
+  export interface Networks {
+      nets: Network[];
+  }
+  const Community: React.FC = () => {
+      const [networks, useNetworks] = useState([]);
+      
+      React.useEffect(() => {
+        const FetchData = async () => {
+            const response = await axios.get('/api/networks');
+            console.log(response.data);
+            console.log("Hey this function is run");
+            useNetworks(response.data);
+        }
+        
+        FetchData();
+      }, []);
+  
+      const renderNetwork = (network: {
+          name: string,
+          description: string,
+          creator: string,
+          id: string,
+          layers: Layer[],
+      }) => {
+          return (
+              <ComNetwork
+                  name={network.name}
+                  description={network.description}
+                  creator={network.creator}
+                  id={network.id}
+                  layers={network.layers}
+              />
+          );
+      };
+  
+      return (
+          <>
+              <Container>
+                  <h1>Community Networks</h1>
+                  {networks.map((net) => renderNetwork(net))}
+              </Container>
+          </>
+      );
+  };
+  
+  export default Community;
