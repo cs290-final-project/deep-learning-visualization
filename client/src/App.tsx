bb155d4e6d127952b175d1a3db5ad1e90b19ab69
ShaurGaur <shauryavrat@live.com>
2020-06-07 17:34:58 -0700

    added browser router for "page separation"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,25 +2,27 @@ import React from "react";
 import { DndProvider } from "react-dnd";
 import Backend from "react-dnd-html5-backend";
 import { Container, Row, Col } from "react-bootstrap";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Network from "./Components/network/Network";
-import Community from "./Community"
+import Community from "./Community";
 
 function App() {
     return (
-        <div className="App">
-            <Container fluid>
-                <Row>
-                    <Col md={4}>
+        <BrowserRouter>
+            <div className="App">
+                <h1>Vis-AI (header a Work in Progress)</h1>
+                <Link to="/">Home   </Link>
+                <Link to="/network">New Network</Link>
+                <Switch>
+                    <Route path="/network">
                         <DndProvider backend={Backend}>
                             <Network />
                         </DndProvider>
-                    </Col>
-                    <Col md={4}>
-                        <Community />
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+                    </Route>
+                    <Route path="/" component={Community} />
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 }
 
